81dc32249d2901b6dfed00c06edd9f8336643a24
juan pablo mendez <pablochileidh@gmail.com>
2023-11-10 11:55:31 -0300

    desarrollo de educación

---
+++ assets/js/scripts.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  var progressBar = document.querySelectorAll(".progress-bar");
+  const time = 1500;
+
+  function calculateTime(time, dataCount) {
+    return time / dataCount;
+  }
+
+  progressBar.forEach(function (i) {
+    let count = 0;
+    let label = i.children[0];
+    let line = i.children[1];
+
+    let dataCount = label.getAttribute("data-count");
+    let lineCount = line.children[0];
+
+    let value = line.style.width.substr(0, line.style.width.length - 1.5) / 79;
+    let runTime = calculateTime(time, dataCount);
+
+    let animationLineCount = setInterval(function () {
+
+      if (count < dataCount) {
+        count++;
+        label.innerHTML = count + "%";
+        lineCount.style.width = count * value + "px";
+      }
+
+    }, runTime)
+
+    console.log(value);
+  });
+});
